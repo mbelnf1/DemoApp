bf3ac9dcfea715308405aa736315dc7deafbc081
mbelnf1 <mbeln1992@gmail.com>
2019-07-29 15:11:03 -0400

    Add project files.

---
+++ DemoApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> demo = new List<string>();
+
+            demo.Add("Bob");
+            demo.Add("George");
+            demo.Add("Tom");
+            demo.Add("Sue");
+
+            //long way
+
+            //for (int i = 0; i < demo.Count; i++)
+            //{
+            //    Console.WriteLine(demo[i]);
+            //}
+
+            //Shorter way and same results
+
+            foreach (string name in demo)
+            {
+                Console.WriteLine(name);
+            }
+            Console.Read();
+        }
+    }
+}
